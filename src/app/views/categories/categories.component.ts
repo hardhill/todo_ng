ec05d2ed41b063d5438f5782e20b9163ef5d5dae
W.Bielecki <bva@sibdigital.net>
2022-03-15 15:42:09 +0800

    отображение категорий в левом меню

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataHandlerService} from "../../service/data-handler.service";
+import {Category} from "../../model/Category";
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
-
-  constructor() { }
+  categories?:Category[]
+  constructor(private dataHandler:DataHandlerService) { }
 
   ngOnInit(): void {
+    this.categories = this.dataHandler.getCategories()
+    console.log(this.categories)
   }
 
 }
