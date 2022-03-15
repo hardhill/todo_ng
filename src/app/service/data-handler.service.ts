06d9d47280df35a93b372ab6aaec9967b55c8720
W.Bielecki <bva@sibdigital.net>
2022-03-15 19:02:10 +0800

    добавление категорий и задач в стиле РП (добавлен BehaviorSubject)

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
 import {Task} from "../model/Task";
-import {Subject} from "rxjs";
+import {BehaviorSubject, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  taskSubject:Subject<Task[]> = new Subject<Task[]>()
+  taskSubject:BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>(TestData.tasks)
+  categorySubject = new BehaviorSubject<Category[]>(TestData.categories)
 
   constructor() { }
 
-  getCategories():Category[]{
-    return TestData.categories
+  fillCategories(){
+    this.categorySubject.next(TestData.categories)
   }
 
   fillTasks(){
